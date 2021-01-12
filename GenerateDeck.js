6bc80c5ce0c0ceb75b36a9b3c69592fecdfddd3a
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-01-12 10:14:12 -0500

    Update GenerateDeck.js

--- GenerateDeck.js
+++ GenerateDeck.js
@@ -1,1 +1,14 @@
+import java.util.*;
 class GenerateDeck {
+  private String mode;
+  private int sReSpecs;
+  private int fReSpecs;
+  private int cardRatio;
+  GenerateDeck() {
+    mode = "STANDARD";
+    sReSpecs = 5;
+    fReSpecs = 3;
+    cardRatio = 
+  }
+  
+  GenerateDeck(String mChoice, 
