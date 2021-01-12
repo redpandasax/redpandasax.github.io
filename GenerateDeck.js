b7fedd9d4bdec1ec49ee243b27ea064b163ae6fd
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-01-12 10:22:18 -0500

    Update GenerateDeck.js

--- GenerateDeck.js
+++ GenerateDeck.js
@@ -1,14 +1,27 @@
 import java.util.*;
-class GenerateDeck {
+class DeckGen {
   private String mode;
   private int sReSpecs;
   private int fReSpecs;
-  private int cardRatio;
-  GenerateDeck() {
+  private int[3] cardRatio;
+  private ArrayList <
+  
+  public DeckGen() {
     mode = "STANDARD";
     sReSpecs = 5;
     fReSpecs = 3;
-    cardRatio = 
+    cardRatio[0] = 2;
+    cardRatio[1] = 2;
+    cardRatio[2] = 1;
   }
   
-  GenerateDeck(String mChoice, 
+  public DeckGen(String mChoice, int[3] ratioChoice) {
+    sReSpecs = 5;
+    fReSpecs = 3;
+    for (int i = 0; i < 3; i ++) {
+      cardRatio[i] = ratioChoice[i];
+    }
+    mode = mChoice;
+  }
+  
+  public 
