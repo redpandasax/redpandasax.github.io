f2f5c7e95791392a296dc058ce38c5deba4fd069
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-06-23 15:32:32 -0400

    Update mapAssist.js

--- mapAssist.js
+++ mapAssist.js
@@ -1,17 +1,19 @@
-//Prints the coordinate of the cursor for the user
-var svg = document.getElementById("mapSVG");
+function ini() {
+  //Prints the coordinate of the cursor for the user
+  var svg = document.getElementById("mapSVG");
 
-// Create an SVGPoint for future math
-var pt = svg.createSVGPoint();
+  // Create an SVGPoint for future math
+  var pt = svg.createSVGPoint();
 
-// Get point in global SVG space
-function cursorPoint(evt){
-  pt.x = evt.clientX; pt.y = evt.clientY;
-  return pt.matrixTransform(svg.getScreenCTM().inverse());
+  // Get point in global SVG space
+  function cursorPoint(evt){
+    pt.x = evt.clientX; pt.y = evt.clientY;
+    return pt.matrixTransform(svg.getScreenCTM().inverse());
+  }
+
+  svg.addEventListener("mousemove",function(evt){
+    var loc = cursorPoint(evt);
+    console.log(Math.floor(loc.x)/100 + " " + Math.floor(loc.y)/100);
+    // Use loc.x and loc.y here
+  },false);
 }
-
-svg.addEventListener("mousemove",function(evt){
-  var loc = cursorPoint(evt);
-  console.log(Math.floor(loc.x)/100 + " " + Math.floor(loc.y)/100);
-  // Use loc.x and loc.y here
-},false);
