ed87f926b8ccd6b476acdf60935baa0e2440a266
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-07-27 18:02:53 -0400

    Update cardGen.js

--- cardGen.js
+++ cardGen.js
@@ -1,5 +1,5 @@
 function layerCard(color, type) {
   var canvas = document.getElementById("card");
-  var ctx = document.getElementById("card").getContext("2d");
+  var ctx = canvas.getContext("2d");
   ctx.drawImage(document.getElementById("layer0"), 0, 0);
 }
