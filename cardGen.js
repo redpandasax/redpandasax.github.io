595bce2820c61f99f02ce74e687d076524455c3b
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-07-28 15:59:06 -0400

    Update cardGen.js

--- cardGen.js
+++ cardGen.js
@@ -1,9 +1,9 @@
 function layerCard(color, type, investCt) {
   var canvas = document.getElementById("card");
   var ctx = canvas.getContext("2d");
-  var l0 = color.concat(" ".concat(type));
-  var l1 = color.concat(" Textboxes");
-  var inv = color.concat(" ".concat(investCt.toString().concat("Invest")));
+  var l0 = color.value.concat(" ".concat(type.value));
+  var l1 = color.value.concat(" Textboxes");
+  var inv = color.value.concat(" ".concat(investCt.toString().concat("Invest")));
   ctx.drawImage(document.getElementById(l0), 0, 0, 2292, 3201);
   ctx.drawImage(document.getElementById(l1), 0, 0, 2292, 3201);
   ctx.drawImage(document.getElementById(inv), 0, 0, 2292, 3201);
