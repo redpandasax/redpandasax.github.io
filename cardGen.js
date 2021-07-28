4c2c0362a2cca35d0a44204b80fa9f802473b2e0
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-07-28 16:52:41 -0400

    Update cardGen.js

--- cardGen.js
+++ cardGen.js
@@ -6,5 +6,6 @@ function layerCard(color, type, investCt) {
   var inv = color.concat(" ".concat(investCt.toString().concat("Invest")));
   ctx.drawImage(document.getElementById(l0), 0, 0, 2292, 3201);
   ctx.drawImage(document.getElementById(l1), 0, 0, 2292, 3201);
-  ctx.drawImage(document.getElementById(inv), 0, 0, 2292, 3201);
+  if (inv > 0)
+    ctx.drawImage(document.getElementById(inv), 0, 0, 2292, 3201);
 }
