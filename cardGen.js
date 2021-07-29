6102331d47a07d25b583f0e1e9d7b86a8aec5c9f
redpandasax <46647277+redpandasax@users.noreply.github.com>
2021-07-29 13:35:00 -0400

    Attempting to import an image

--- cardGen.js
+++ cardGen.js
@@ -1,10 +1,11 @@
-function layerCard(color, type, investCt) {
+function layerCard(color, type, investCt, cardArt) {
   var canvas = document.getElementById("card");
   var ctx = canvas.getContext("2d");
   var l0 = color.concat(" ".concat(type));
   var l1 = color.concat(" Textboxes");
   var inv = color.concat(" ".concat(investCt.toString().concat("Invest")));
   ctx.drawImage(document.getElementById(l0), 0, 0, 2292, 3201);
+  ctx.drawImage(cardArt, 205, 545, 1900, 1070)
   ctx.drawImage(document.getElementById(l1), 0, 0, 2292, 3201);
   if (investCt > 0)
     ctx.drawImage(document.getElementById(inv), 0, 0, 2292, 3201);
